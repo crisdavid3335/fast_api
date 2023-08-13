d2b764c668d541bb7207387441bc2961ab711235
Chris PC <crisdavid3335@gmail.com>
2023-08-13 19:35:02 -0300

    asedf

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = 'Mi app con platzi'
+app.version = '0.0.1'
 
-@app.get('/')
+@app.get('/', tags=['home'])
 def message():
+
+    
     return 'Hola mundo'
